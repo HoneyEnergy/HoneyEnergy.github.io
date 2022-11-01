016073a963f79030e40ee6f29d77d51fe32f12ef
intel <you@example.com>
2022-11-01 12:33:27 +0100

    typejs and nodejs

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-//alert("Welcome to JATO'S Offial Website")
+alert("Welcome to JATO'S Offial Website")
 import Typed from 'typed.js';
 console.clear()
-src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"
-
+var typed = new Typed(".type", {
+    strings: ['What is JOTA '],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+    })
