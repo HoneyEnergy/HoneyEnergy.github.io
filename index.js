4a9d6aac8cf075b19332cc69353db34a06ef9e78
forceCpp <mrinit@gh.config>
2022-11-08 22:30:05 +0100

    indwx

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-alert("Welcome to JATO'S Offial Website")
-import Typed from 'typed.js';
-console.clear()
-var typed = new Typed(".type", {
-    strings: ['What is JOTA '],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true,
-    })
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+  
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+      alert("Auf IOS geht diese site villeicht nicht wir emphelen diese das die es auf en pc versuchen")
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+      alert("Auf Android geht diese site villeicht nicht wir emphelen diese das die es auf en pc versuchen")
+    } else if (!os && /Linux/.test(platform)) {
+      os = 'Linux';
+    }
+  
+    return os;
+  }
+getOS()
+
+if (getOS() == 'Android') {
+    alert("its better if you vist this website from a pc")
+}
+if (getOS() == 'iOS') {
+    alert("its better if you vist this website from a pc")
+}
